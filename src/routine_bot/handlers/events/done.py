baa21411ba3f44dd8e8bb114fea3d26b9e3e36cb
acaibowlz <hankthedev@gmail.com>
2026-01-10 15:03:24 +0800

    fix: contextualized logger for done event

--- src/routine_bot/handlers/events/done.py
+++ src/routine_bot/handlers/events/done.py
@@ -14,24 +14,29 @@ from routine_bot.constants import TZ_TAIPEI
 from routine_bot.enums.chat import ChatStatus, ChatType
 from routine_bot.enums.steps import DoneEventSteps
 from routine_bot.errors import EventNotFoundError, InvalidStepError
-from routine_bot.logger import format_logger_name, validate_event_name
+from routine_bot.logger import add_context, format_logger_name, indent, shorten_uuid
 from routine_bot.models import ChatData, RecordData
+from routine_bot.utils import validate_event_name
 
 logger = logging.getLogger(format_logger_name(__name__))
 
 
 def _process_event_name(text: str, chat: ChatData, conn: psycopg.Connection) -> TemplateMessage | FlexMessage:
-    logger.info("Processing event name")
+    cxt_logger = add_context(logger, chat_id=chat.chat_id)
+    cxt_logger.debug("Processing event name")
     event_name = text
+
     error_msg = validate_event_name(event_name)
     if error_msg is not None:
-        logger.info(f"Invalid event name. Input: {event_name}, Error msg: {''.join(error_msg)}")
+        cxt_logger.debug("Invalid event name: %r, error msg=%s", event_name, "".join(error_msg))
         return msg.error.error(error_msg)
-    event_id = event_db.get_event_id(chat.user_id, event_name, conn)
+    user_id = chat.user_id
+    event_id = event_db.get_event_id(user_id, event_name, conn)
     if event_id is None:
-        logger.info(f"Event not found: {event_name}")
+        cxt_logger.debug("Event not found: user_id=%s, event_name=%r", user_id, event_name)
         return msg.error.event_name_not_found(event_name)
 
+    cxt_logger.info("Event selected: %r (%s)", event_name, shorten_uuid(event_id))
     chat_db.set_chat_current_step(chat.chat_id, DoneEventSteps.SELECT_DONE_DATE.value, conn)
     chat.payload = chat_db.patch_chat_payload(
         chat=chat,
@@ -46,21 +51,21 @@ def _process_event_name(text: str, chat: ChatData, conn: psycopg.Connection) -> 
 def process_selected_done_date(
     postback: PostbackEvent, chat: ChatData, conn: psycopg.Connection
 ) -> TemplateMessage | FlexMessage:
-    logger.info("Processing selected done date")
+    cxt_logger = add_context(logger, chat_id=chat.chat_id)
+    cxt_logger.debug("Processing new done date")
     if postback.postback.params is None:
         raise AttributeError("Postback contains no data")
 
     done_at = datetime.strptime(postback.postback.params["date"], "%Y-%m-%d")
     done_at = done_at.replace(tzinfo=TZ_TAIPEI)
-    logger.info(f"Selected done date: {done_at}")
-    if done_at > datetime.today().astimezone(tz=TZ_TAIPEI):
-        logger.info("Date exceeds today")
+    today = datetime.today().astimezone(tz=TZ_TAIPEI)
+    cxt_logger.info(f"New done date set to {done_at}")
+    if done_at > today:
+        cxt_logger.debug("Done date exceeds today: %s > %s", done_at, today)
         return msg.events.done.invalid_done_date_selected_exceeds_today(chat.payload)
 
     event_id = chat.payload["event_id"]
     event = event_db.get_event_by_id(event_id, conn)
-    if event is None:
-        raise EventNotFoundError(f"Event not found: {event_id}")
     record_id = str(uuid.uuid4())
     record = RecordData(
         record_id=record_id,
@@ -69,17 +74,23 @@ def process_selected_done_date(
         user_id=chat.user_id,
         done_at=done_at,
     )
-    logger.info("┌── Creating New Record ────────────────────")
-    logger.info(f"│ ID: {record_id}")
-    logger.info(f"│ User: {chat.user_id}")
-    logger.info(f"│ Name: {event.event_name}")
-    logger.info(f"│ Done At: {done_at.astimezone(UTC)}")
-    logger.info("└───────────────────────────────────────────")
     record_db.add_record(record, conn)
-
     if done_at > event.last_done_at:
-        logger.info("Updating event's last done date: The new done date is more recent")
+        logger.info("Updating event's latest done date")
         event_db.set_event_last_done_at(event_id, done_at, conn)
+
+    summary = "\n".join(
+        [
+            "┌── New Record ─────────────────────────────",
+            f"│ Record ID: {record_id}",
+            f"│ User: {event.user_id}",
+            f"│ Event Name: {event.event_name}",
+            f"│ Event ID: {event_id}",
+            f"│ New Done Date: {done_at.astimezone(UTC)}",
+            "└───────────────────────────────────────────",
+        ]
+    )
+    cxt_logger.info("Record created successfully\n%s", indent(summary))
 
     chat.payload = chat_db.patch_chat_payload(
         chat=chat,
@@ -93,8 +104,8 @@ def process_selected_done_date(
 
 def create_done_event_chat(user_id: str, conn: psycopg.Connection) -> FlexMessage:
     chat_id = str(uuid.uuid4())
-    logger.info("Creating new chat, chat type: done event")
-    logger.info(f"Chat ID: {chat_id}")
+    cxt_logger = add_context(logger, chat_id=chat_id)
+    cxt_logger.info("New chat created: user=%s, chat_type=done_event", shorten_uuid(user_id))
     chat = ChatData(
         chat_id=chat_id,
         user_id=user_id,
@@ -108,6 +119,8 @@ def create_done_event_chat(user_id: str, conn: psycopg.Connection) -> FlexMessag
 
 
 def handle_done_event_chat(text: str, chat: ChatData, conn: psycopg.Connection) -> TemplateMessage | FlexMessage:
+    cxt_logger = add_context(logger, chat_id=chat.chat_id)
+    cxt_logger.debug("Routing done event chat: step=%s, text=%r", chat.current_step, text)
     handlers = {
         DoneEventSteps.ENTER_NAME.value: _process_event_name,
         DoneEventSteps.SELECT_DONE_DATE.value: lambda text, chat, conn: msg.events.done.invalid_text_input(
